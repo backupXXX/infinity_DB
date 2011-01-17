8c1d77db16065d73e742c11cde0a9cc8a99c9ae1
GMHAZE <iceis2@hotmail.com>
2011-01-17 09:29:56 -0500

    sql fix

--- MaNGOS_Updates/47m_MaNGOS_Quest_DATABASE_Mining.sql
+++ MaNGOS_Updates/47m_MaNGOS_Quest_DATABASE_Mining.sql
@@ -8,6 +8,7 @@ DELETE FROM `gameobject_involvedrelation` WHERE `quest` = 12860;
 INSERT INTO `creature_involvedrelation` (`id`, `quest`) VALUES (29579, 12860);
 UPDATE `creature_template` SET `npcflag`=`npcflag`|2 WHERE `entry`=29579;
 UPDATE `quest_template` SET `ReqCreatureOrGOId1` = '29746' WHERE `entry` = 12860;
+UPDATE `quest_template` SET `ReqItemCount1` = 1, `ReqItemCount2` = 0 WHERE `entry` = 12860;
 
 -- FallenAngelX
 ALTER TABLE db_version_Infinity_Update CHANGE COLUMN r46 r47 bit;
