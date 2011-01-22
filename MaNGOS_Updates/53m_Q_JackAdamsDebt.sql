3a2aac7dd524793572ddc83d8f62adb8480173d9
GMHAZE <iceis2@hotmail.com>
2011-01-22 14:39:58 -0500

    added waterful sql stuff and typo in 53m_sql so reun it first 8)

--- MaNGOS_Updates/53m_Q_JackAdamsDebt.sql
+++ MaNGOS_Updates/53m_Q_JackAdamsDebt.sql
@@ -6,3 +6,9 @@ INSERT INTO creature_addon (guid,mount,bytes1,b2_0_sheath,b2_1_pvp_state,emote) 
 UPDATE creature_template SET ScriptName = 'npc_olga', AIName = '' WHERE entry = 24639;
 UPDATE creature_template SET ScriptName = 'npc_jack_adams', AIName = '' WHERE entry = 24788;
 
+-- FallenAngelX
+ALTER TABLE db_version_Infinity_Update CHANGE COLUMN r52 r53 bit;
+REPLACE INTO `db_version_Infinity_Update` (`version`) VALUES ('r53');
+
+UPDATE db_version SET `cache_id`= 'r53';
+UPDATE db_version SET `version`= 'YTDB581_Infinity_Update_r53';
