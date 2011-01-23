b820956df5fe7acfd91b61df03214beffd78da29
GMHAZE <iceis2@hotmail.com>
2011-01-23 12:04:25 -0500

    setting up for new all up to date zips  ...

--- ScriptDev2_Updates/SD2_46m_scriptdev2_.sql
+++
@@ -1,9 +0,0 @@
--- Eitrigg Bow Emote handler
-DELETE FROM script_texts WHERE entry IN (-1999899);
-INSERT INTO script_texts (`entry`,`content_default`,`sound`,`type`,`emote`,`comment`) VALUES
-(-1999899,'%s bows his head for a moment, as if in prayer.',0,2,2,'Eitrigg bow in prayer'); -- Bow
-
-
-DELETE FROM script_texts WHERE entry = -1998992;
-INSERT INTO script_texts (`entry`,`content_default`,`comment`) VALUES
-(-1998992, 'As you wish ...','Old Ironbark - Dire Maul - start walking');
